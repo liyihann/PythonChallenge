b55425cd50e2622565ec690581763f2cf21b079c
liyihan <liyihan@outlook.com>
2018-07-12 16:41:45 +0800

    level 4

--- level4.py
+++ level4.py
@@ -1,1 +1,17 @@
 import urllib
+import re
+def nextPage(p):
+    url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='+p
+    str = urllib.request.urlopen(url).read().decode('utf-8')
+    try:
+        newP = re.search(r'([0-9]+)', str).group(1)
+        return newP
+    except:
+        print(str)
+        return ''
+
+p = '12345'
+for i in range(1,400):
+    print('{}:{}'.format(i,p))
+    p = nextPage(p)
+# peak.html
